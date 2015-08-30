52a9fb5ec3ccfaa5dc0225d7c598649bf91d92fa
Igor Shapiro <shapigor@gmail.com>
2015-08-30 04:11:13 -0700

    Added failed messages loggin

--- config/init.js
+++ config/init.js
@@ -1,7 +1,10 @@
 var bunyan = require('bunyan')
 global.log = bunyan.createLogger({
   name: "service_hub",
-  serializers: bunyan.stdSerializers
+  serializers: bunyan.stdSerializers,
+  streams: [{
+    path: __dirname + '/../hub.log'
+  }]
 })
 var StatsD = require('node-statsd')
 global.stats = new StatsD()
