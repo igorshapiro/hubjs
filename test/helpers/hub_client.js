8096d6e05762581c6c0e5b25f5e71ece96685fd5
Igor Shapiro <shapigor@gmail.com>
2015-08-30 03:56:34 -0700

    Removed usages of IOJS features

--- test/helpers/hub_client.js
+++ test/helpers/hub_client.js
@@ -24,7 +24,7 @@ module.exports = {
 
   getProcessingMessages: function*(serviceName) {
     return handleHttpResponse(
-      rest.get(`http://localhost:8080/api/v1/${serviceName}/processing`)
+      rest.get("http://localhost:8080/api/v1/" + serviceName + "/processing")
     )
   }
 }
