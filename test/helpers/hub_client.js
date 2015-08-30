34c43e0e56095c84ab6ee1f67c13a8061c2759ae
Igor Shapiro <shapigor@gmail.com>
2015-08-29 20:31:13 -0700

    Implement /api/v1/:service/processing endpoint

--- test/helpers/hub_client.js
+++ test/helpers/hub_client.js
@@ -1,19 +1,30 @@
 var rest = require('restler')
 
+function handleHttpResponse(req) {
+  return new Promise(function(resolve, reject) {
+    req.on('success', function(data, response) {
+      var success = [200, 201].indexOf(response.statusCode) != -1
+      success ? resolve(data) : reject(new Error(response))
+    })
+    .on('error', function(err, response) {
+      reject(new Error(err))
+    })
+    .on('fail', function(data, response) {
+      reject(new Error(data))
+    })
+  })
+}
+
 module.exports = {
   sendMessage: function*(msg) {
-    return new Promise(function(resolve, reject) {
-      rest
-        .postJson("http://localhost:8080/api/v1/messages", msg)
-        .on('success', function(data, response) {
-          response.statusCode == 201 ? resolve() : reject(response)
-        })
-        .on('error', function(err, response) {
-          reject(err)
-        })
-        .on('fail', function(data, response) {
-          reject(data)
-        })
-    })
+    return handleHttpResponse(
+      rest.postJson("http://localhost:8080/api/v1/messages", msg)
+    )
+  },
+
+  getProcessingMessages: function*(serviceName) {
+    return handleHttpResponse(
+      rest.get(`http://localhost:8080/api/v1/${serviceName}/processing`)
+    )
   }
 }
--- test/integration/basic.js
+++ test/integration/basic.js
@@ -1,6 +1,7 @@
 describe('ServiceHub', function() {
   var svcHost = "http://localhost:3100"
   var CONCURRENCY = 5
+  var TEN_SECONDS = 10000
 
   var hub = null
   before(function*() {
@@ -22,28 +23,51 @@ describe('ServiceHub', function() {
 
   afterEach(nock.cleanAll)
 
-  function mockEndpoint(path, status, msg, times) {
-    var req = nock(svcHost).post(path)
-    if (times)
-      req = req.times(times)
-    return req.reply(status, msg || "")
+  function mockEndpoint(options) {
+    var req = nock(svcHost).post(options.path)
+    if (options.times) req = req.times(options.times)
+    if (options.delay) req = req.delay(options.delay)
+    return req.reply(options.status || 200, options.msg || "Ok")
   }
 
+  describe ("api/processing", function() {
+    it("returns messages being processed", function*() {
+      mockEndpoint({path: "/will_succeed", delay: 50})
+      var msgId = yield hubClient.sendMessage({type: 'will_succeed'})
+
+      yield expect(function*() {
+        var msgs = yield hubClient.getProcessingMessages("sub")
+        return _.isEqual(msgs.stats, {total: 1}) &&
+          _.isEqual(msgs.messages, [
+            {type: 'will_succeed', id: msgId, maxAttempts: 5, attemptsMade: 0}
+          ])
+      }).within(30).to.become(true)
+
+      yield expect(function*() {
+        var msgs = yield hubClient.getProcessingMessages("sub")
+        return _.isEqual(msgs.stats, {total: 0}) &&
+          _.isEqual(msgs.messages, [])
+      }).within(100).to.become(true)
+
+    })
+  })
+
   it ("Doesn't send more than `concurrency` messages", function*() {
-    var TEN_SECONDS = 10000
-
     var counter = 0
-    var req = mockEndpoint("/will_succeed", function(uri, request, cb) {
-      counter++
-      setTimeout(function() {
-        cb(null, [200, "Ok"])
-      }, TEN_SECONDS)
-    }, "", 10)
+    var req = mockEndpoint({
+      path: "/will_succeed",
+      status: function(uri, request, cb) {
+        counter++
+        setTimeout(function() {
+          cb(null, [200, "Ok"])
+        }, TEN_SECONDS)
+      },
+      times: 10
+    })
 
-
-    yield _(CONCURRENCY * 2).range().map(function() {
-      return hubClient.sendMessage({type: 'will_succeed'})
-    }).value()
+    for (var i = 0; i < CONCURRENCY * 2; i++) {
+      yield hubClient.sendMessage({type: 'will_succeed'})
+    }
 
     yield expect(function() {
       return counter == CONCURRENCY
@@ -51,13 +75,13 @@ describe('ServiceHub', function() {
   })
 
   it ("Delivers message to service", function*() {
-    var req = mockEndpoint('/will_succeed', 200, 'Ok')
+    var req = mockEndpoint({path: '/will_succeed'})
     yield hubClient.sendMessage({type: 'will_succeed'})
     yield expect(req.isDone).to.within(200).become(true)
   })
 
   it ("Puts message in dead letter if maxAttempts reached", function*() {
-    var req = mockEndpoint('/will_fail', 500, 'Failure', 5)
+    var req = mockEndpoint({path: '/will_fail', status: 500, msg: 'Failure', times: 5})
     yield hubClient.sendMessage({type: 'will_fail'})
 
     yield expect(function*(){
@@ -67,7 +91,7 @@ describe('ServiceHub', function() {
   })
 
   it ("Re-delivers message to service if 2xx or 3xx not returned", function*() {
-    var req = mockEndpoint('/will_fail', 500, 'Failure', 5)
+    var req = mockEndpoint({path: '/will_fail', status: 500, msg: 'Failure', times: 5})
     yield hubClient.sendMessage({type: 'will_fail'})
     yield expect(function() { return req.pendingMocks.length == 0 } )
       .within(200).to.become(true)
