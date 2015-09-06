2a9083b04d0d069797cf3df21e8bb0accd8468da
Igor Shapiro <shapigor@gmail.com>
2015-09-07 00:40:08 +0300

    UI with delete and reenqueue

--- frontend/app/adapters/application.js
+++ frontend/app/adapters/application.js
@@ -1,6 +1,5 @@
 import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
-  namespace: "api/v1",
-  host: "http://localhost:8080"
+  namespace: "api/v1"
 });
--- frontend/app/adapters/message.js
+++ frontend/app/adapters/message.js
@@ -4,7 +4,7 @@ export default ApplicationAdapter.extend({
   buildURL: function(model, id, snapshot, requestType, query) {
     if (model == "message" && requestType == "deleteRecord") {
       var params = snapshot.get('metaParams')
-      var url = this.host + "/" + this.namespace + "/messages/" +
+      var url = "/" + this.namespace + "/messages/" +
         params.service + "/" + params.type + "/" + snapshot.id
       if (params.reenqueue)
         url += "?reenqueue=true"
