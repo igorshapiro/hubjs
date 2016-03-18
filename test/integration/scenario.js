84e169d8ea0098f84e1620500c8f0faf702a3710
Igor Shapiro <shapigor@gmail.com>
2016-03-19 00:54:43 +0200

    + Implemented archive middleware + Allow setting storage on manifest level + Fix message API to return 201 instead of 204

--- test/integration/scenario.js
+++ test/integration/scenario.js
@@ -146,7 +146,7 @@ class ScenarioBuilder {
 
   handleAPICallError(endpoint, response) {
     var status = response.statusCode
-    if (status !== 204) throw new Error(
+    if (status !== 201) throw new Error(
       `POST ${endpoint} responded with ${status}:\r\n${response.body}`
     )
   }
@@ -265,6 +265,7 @@ class ScenarioBuilder {
     var LockManager = require('./../../lib/middlewares/lock_manager')
     var Recurring = require('./../../lib/middlewares/recurring')
     var Processing = require('./../../lib/middlewares/processing')
+    var Archive = require('./../../lib/middlewares/archive')
 
     // Used for launching multiple hub instances
     var port = this.basePort + (options.instanceNumber || 0)
@@ -282,6 +283,7 @@ class ScenarioBuilder {
         { type: LockManager },
         { type: Recurring, params: { pollingIntervalMillis: 50 } },
         { type: Processing },
+        { type: Archive }
       ]
     }
   }
